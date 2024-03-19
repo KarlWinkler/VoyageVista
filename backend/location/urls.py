b20b52ed654aa6e0a31890b71f8b3b698c1a2b47
karlWinkler <karlw200@gmail.com>
2024-03-19 13:11:05 -0600

    fix: correct location path search order

--- backend/location/urls.py
+++ backend/location/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 
 router = routers.SimpleRouter()
-router.register(r'', views.LocationViewSet)
 router.register(r'images', views.LocationImageViewSet)
 router.register(r'ratings', views.RatingViewSet)
 router.register(r'comments', views.CommentViewSet)
 router.register(r'tags', views.LocationTagViewSet)
+router.register(r'', views.LocationViewSet)
 
 urlpatterns = router.urls
