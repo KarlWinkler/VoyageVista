6810508afbad882455876f36e3fcb2a917a3cccf
karlWinkler <karlw200@gmail.com>
2024-03-19 13:43:22 -0600

    feat: add tag to users

--- backend/authentication/admin.py
+++ backend/authentication/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import BucketList, Visited, UserTag
 
-# Register your models here.
+admin.site.register(BucketList)
+admin.site.register(Visited)
+admin.site.register(UserTag)
--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from django.contrib.auth.models import User
 from location.models import Location
+from tag.models import Tag
 
 class BucketList(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -20,3 +21,12 @@ class Visited(models.Model):
 
     def __str__(self):
         return self.location.name
+
+class UserTag(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username + ' - ' + self.tag.name
