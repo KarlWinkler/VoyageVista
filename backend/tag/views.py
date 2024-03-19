8cf85f8be1420157857c05433502b97813d57aec
karlWinkler <karlw200@gmail.com>
2024-03-19 13:43:06 -0600

    fix: add tag urls to the app urls

--- backend/tag/views.py
+++ backend/tag/views.py
@@ -8,18 +8,5 @@ from .forms import TagForm
 
 
 class TagViewSet(viewsets.ModelViewSet):
-	def addTag(request):
-		form = TagForm
-		if request.method == 'POST':
-			form = TagForm(request.POST)
-			if form.is_valid():
-				form.save()
-		context = {'form':form}
-		return render(request, 'add-tag.html', context)
-		
-	def retrieve(self, request, pk=None):
-		queryset = Tag.objects.all()
-		locationTag = get_object_or_404(queryset, pk=pk)
-		serializer_class = TagSerializer
-		return Response(serializer_class.data)
-
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
